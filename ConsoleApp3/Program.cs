aa200922a5e00ab7920b92c0acdea08ec6659bc7
leit07001 <leit07001@E-PW05C37S>
2024-11-14 12:47:52 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Har du gått ut gymnasiet? (j/n): ");
+        char harGottUtGymnasiet = char.ToLower(Console.ReadKey().KeyChar); 
+        Console.WriteLine(); 
+
+       
+        Console.Write("Hur gammal är du? ");
+        int ålder = int.Parse(Console.ReadLine());
+
+        if (harGottUtGymnasiet == 'j' && ålder < 22)
+        {
+            Console.WriteLine("Vi vill gärna anställa dig");
+        }
+        else
+        {
+            Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+        }
+    }
+}
